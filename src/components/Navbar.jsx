486689bebd633e81b7fab6df9e26d119cfcd43f4
Chris <zabelca01@gmail.com>
2023-10-12 15:42:25 +0000

    added logo to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from 'next/link'
 import React, { useState } from 'react'
+import Image from 'next/image';
 import NavLink from './NavLink';
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from './MenuOverlay';
@@ -27,8 +28,18 @@ const Navbar = () => {
         <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
             <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
                 <Link href={"/"} className="text-2xl md:text-5xl text-white font-semibold">
-                    LOGO
-                </Link>
+                    <div className='fixed transform -translate-y-1/2'>
+                        <Image
+                            src="/images/Blazetek-logos_white.png"
+                            alt="blazetek-logo"
+                            height={50}
+                            width={200}
+                            className=''
+                            //fill={true}
+                            //objectFit="cover"
+                        />
+                    </div>
+                </Link> 
                 <div className="mobile-menu block md:hidden">
                     {!navbarOpen ? (
                         <button onClick={() => setNavbarOpen(true)} className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white">
