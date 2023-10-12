67cdb1aa3b14502885009e65e54de78c9abfb604
Chris <zabelca01@gmail.com>
2023-10-12 15:43:39 +0000

    fixed typo

--- src/components/EmailSection.jsx
+++ src/components/EmailSection.jsx
@@ -45,7 +45,7 @@ const EmailSection = () => {
         </h5>
         <p className='text-[#ADB7BE] mb-4 max-w-md'>
           {" "}
-          I'm currently looking for new opportunities, my indox is always open.
+          I'm currently looking for new opportunities, my inbox is always open.
           Whether you have a question or just want to say hi, I'll try my best
           to get back to you!
         </p>
