b988c5ecd437817117aa5b6e703b4f468a11814a
Chris <zabelca01@gmail.com>
2023-10-12 19:44:56 +0000

    added logo to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Footer = () => {
   return (
     <footer className='footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white'>
       <div className='container p-12 flex justify-between'>
         <span>
-          LOGO
+          <div className='absolute transform -translate-y-1/2'>
+            <Image
+              src="/images/Blazetek-logos_white.png"
+              alt="blazetek-logo"
+              height={50}
+              width={125}
+              className=''
+              //fill={true}
+              //objectFit="cover"
+            />
+          </div>
         </span>
         <p className='text-slate-600'>
           All rights reserved.
